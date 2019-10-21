bb8e2cdc34b25cfac06fff0625473d2e9ad5a707
ArtyomPlay <artglz63@gmail.com>
2019-10-21 16:58:58 +0300

    headers etc.

---
+++ components/typography/VH1.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="h1" :style="style">
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+import typography from '../../mixins/typography';
+
+export default {
+  mixins: [typography],
+  data() {
+    return {
+        defaultWidth: 600
+    };
+  }
+};
+</script>
+
+<style scoped>
+.h1 {
+  font-size: 40px;
+}
+</style>
