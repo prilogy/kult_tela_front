6eaffbfe80c39956d7d52ad1d1b11c01eb252a36
Lukyanov Artyom <artglz63@gmail.com>
2019-12-13 18:43:33 +0300

    icons and html tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
    ** Headers of the page
    */
   head: {
+    htmlAttrs: {
+      lang: 'ru'
+    },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
