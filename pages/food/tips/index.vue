fb661b0e14808d6ea512ecda4b8b46e557eff901
Artyom Lukyanov <artglz63@gmail.com>
2021-03-18 14:13:51 +0300

    sort fix

--- pages/food/tips/index.vue
+++ pages/food/tips/index.vue
@@ -15,7 +15,7 @@
           list__item: true,
           'list__item--disabled': item.minPlan && item.minPlan > CURRENT_PLAN
         }"
-        v-for="item in cats.sort((a,b) => a.id >= b.id ? 1 : -1)"
+        v-for="item in cats"
         :key="item.id"
         @click="goTo(item)"
       >
@@ -31,92 +31,90 @@
 </template>
 
 <script>
-  import VTipSmall from '../../../components/ui/VTipSmall'
+import VTipSmall from '../../../components/ui/VTipSmall'
 
-  export default {
-    middleware: 'requireSub',
-    components: { VTipSmall },
-    data() {
-      return {
-        categories: [
-          {
-            id: 0,
-            title: 'Введение по персональному меню',
-            url: '/intro',
-            minPlan: 1
-          },
-          {
-            id: 1,
-            title: 'Основы питания',
-            url: '/nutrients'
-          },
-          {
-            id: 2,
-            title: 'Советы от диетолога',
-            url: '/videos'
-          },
-          {
-            id: 3,
-            title: 'Советы генерала похудения',
-            url: '/videos/30',
-            minPlan: 1
-          },
-          {
-            id: 4,
-            title: 'Помощник похудения',
-            url: '/kilolight'
-          }
-        ]
-      }
+export default {
+  middleware: 'requireSub',
+  components: { VTipSmall },
+  data() {
+    return {
+      categories: [
+        {
+          id: 0,
+          title: 'Введение по персональному меню',
+          url: '/intro',
+          minPlan: 1
+        },
+        {
+          id: 1,
+          title: 'Основы питания',
+          url: '/nutrients'
+        },
+        {
+          id: 2,
+          title: 'Советы от диетолога',
+          url: '/videos'
+        },
+        {
+          id: 3,
+          title: 'Советы генерала похудения',
+          url: '/videos/30',
+          minPlan: 1
+        },
+        {
+          id: 4,
+          title: 'Помощник похудения',
+          url: '/kilolight'
+        }
+      ]
+    }
+  },
+  methods: {
+    goTo(item) {
+      if (item.minPlan && item.minPlan > this.CURRENT_PLAN) {
+        this.$store.dispatch('popup/SET_LOCKED', true)
+      } else this.$router.push('/food/tips' + item.url)
+    }
+  },
+  computed: {
+    cats() {
+      return this.categories.sort((a, b) => a.id >= b.id ? 1 : -1)
     },
-    methods: {
-      goTo(item) {
-        if (item.minPlan && item.minPlan > this.CURRENT_PLAN) {
-          this.$store.dispatch('popup/SET_LOCKED', true)
-        } else this.$router.push('/food/tips' + item.url)
-      }
-    },
-    computed: {
-      cats() {
-        return this.categories.sort((a, b) =>
-          a.minPlan && a.minPlan > this.CURRENT_PLAN ? 1 : -1
-        )
-      },
-      CURRENT_PLAN() {
-        return this.$store.state.user.user.plan_id
-      }
+    CURRENT_PLAN() {
+      return this.$store.state.user.user.plan_id
     }
   }
+}
 </script>
 
 <style scoped>
-  .list {
-    margin-bottom: var(--space);
-  }
+.list {
+  margin-bottom: var(--space);
+}
 
-  .list__item {
-    padding: var(--space-third);
-    cursor: pointer;
-    border-radius: var(--radius-half);
-  }
+.list__item {
+  padding: var(--space-third);
+  cursor: pointer;
+  border-radius: var(--radius-half);
+}
 
-  .list__item:hover,
-  .list__item:active,
-  .list__item:focus {
-    background: var(--white-trans4);
-  }
+.list__item:hover,
+.list__item:active,
+.list__item:focus {
+  background: var(--white-trans4);
+}
 
-  .list__item--disabled {
-    cursor: default;
-  }
+.list__item--disabled {
+  cursor: default;
+}
 
-  .list__item--disabled >>> h3 {
-    color: var(--grey-light2) !important;
-  }
+.list__item--disabled >>> h3 {
+  color: var(--grey-light2) !important;
+}
 
-  .list__item--disabled:hover,
-  .list__item--disabled:active,
-  .list__item--disabled:focus {
-    background: unset;
-  }
+.list__item--disabled:hover,
+.list__item--disabled:active,
+.list__item--disabled:focus {
+  background: unset;
+}
 </style>
